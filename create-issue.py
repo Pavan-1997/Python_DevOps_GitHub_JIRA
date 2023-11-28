b57c8ba7e58080687c564581c1b2643b294b065e
Seeramdas Pavan Raj <pavanraj97@gmail.com>
2023-11-28 10:08:38 -0400

    Update create-issue.py

--- create-issue.py
+++ create-issue.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://sqlonlinux.atlassian.net/rest/api/3/issue"
 
-auth = HTTPBasicAuth("sqlonlinux@gmail.com", "ATATT3xFfGF0B0zAbZ2vNQ-y7srBrGQWoZSWR8zgvLHD1cJwklbNcEGdnHBuFNOuq6kRT6fed2LpYLKSmeWkbsYxp7vb-M0KW8Z1b_O0ihyMid4kdpNPBrs4hgV2GXQ_Z1cYkHvHy5HmQeEZ0R1w5GyfZfev0Q-8s5UL-Az2EcQsp32-ctSNA2U=589E62A2")
+auth = HTTPBasicAuth("sqlonlinux@gmail.com", "<API-Token")
 
 headers = {
   "Accept": "application/json",
